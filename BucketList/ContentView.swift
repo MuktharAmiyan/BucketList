df22cc51d599c24f8bcc4af0bc7a0eee3d363baa
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-01-06 11:52:21 +0530

    Local Authentication

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -4,30 +4,41 @@
 //
 //  Created by Mukthar Amiyan on 06/01/24.
 //
-import MapKit
+
+import LocalAuthentication
 import SwiftUI
 
-struct Location :Identifiable {
-    let id = UUID()
-    var name :String
-    var coordinate : CLLocationCoordinate2D
-}
-
-
+ 
 struct ContentView: View {
-     
+    @State private var isUnlocked = false
     var body: some View {
         VStack{
-            MapReader{ proxy in
-                Map()
-                    .onTapGesture { position in
-                        if let coordinate = proxy.convert(position, from: .local ){
-                            print(coordinate)
-                        }
-                    }
+            if isUnlocked {
+                Text("Unlocked")
+            }else{
+                Text("Locked")
             }
         }
-        
+        .onAppear(perform: authenticate)
+    }
+    
+    
+    func authenticate() {
+        let context = LAContext()
+        var error : NSError?
+       
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let reason = "We need to unlock your data."
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                }else{
+                    //There was a problem
+                }
+            }
+        }else{
+            //No biometric
+        }
     }
 }
 
