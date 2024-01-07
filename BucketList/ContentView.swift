ec7a8a3776f2e675af33306947816bebe0e4af4c
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-01-07 11:16:05 +0530

    Adding conformance to Comparable for custom types

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,41 +5,37 @@
 //  Created by Mukthar Amiyan on 06/01/24.
 //
 
-import LocalAuthentication
+
 import SwiftUI
 
+
+struct User: Comparable, Identifiable {
+    
+    let id = UUID()
+    var firstName : String
+    var lastName  : String
+    
+    static func < (lhs : User,rhs : User)  -> Bool {
+        lhs.firstName < rhs.firstName
+    }
+}
  
 struct ContentView: View {
-    @State private var isUnlocked = false
+    let users = [
+    User(firstName: "Mukthar", lastName: "Amiyan"),
+    User(firstName: "Mohammed", lastName: "Danish"),
+    User(firstName: "Mohammed", lastName: "Shibili"),
+    User(firstName: "Mohammed", lastName: "Jasin"),
+    User(firstName: "Dilshad", lastName: "Badusha")
+    ].sorted()
+    
     var body: some View {
-        VStack{
-            if isUnlocked {
-                Text("Unlocked")
-            }else{
-                Text("Locked")
-            }
-        }
-        .onAppear(perform: authenticate)
-    }
-    
-    
-    func authenticate() {
-        let context = LAContext()
-        var error : NSError?
-       
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
-            let reason = "We need to unlock your data."
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                if success {
-                    isUnlocked = true
-                }else{
-                    //There was a problem
-                }
-            }
-        }else{
-            //No biometric
+        List(users){ user in
+            Text(user.firstName + " " + user.lastName)
+            
         }
     }
+    
 }
 
 #Preview {
